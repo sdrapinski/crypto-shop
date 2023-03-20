9f6d5e281f1a87e1336e9dbe6ce7a2e673ab4619
sdrapinski <55097897+sdrapinski@users.noreply.github.com>
2023-03-20 23:07:14 +0100

    update
    s

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import Navbar from "../../components/Navbar";
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  useEffect(() => {
+    axios
+      .get(`${backendUrl}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then((resp) => {
+        console.log(resp);
+      });
+
+    return () => {};
+  }, []);
+
+  return <div>hej</div>;
 };
 
 export default Homepage;
