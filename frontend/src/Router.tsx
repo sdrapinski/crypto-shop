936578b41bf398a166f96044c0e59e6d42f8184f
pkdev-git <admin@pkdevelopment.net>
2023-03-27 15:46:09 +0200

    bootstrap css added

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -5,7 +5,7 @@ import Homepage from "./pages/homepage/Homepage";
 import LoginPage from "./pages/loginPage/LoginPage";
 import RegisterPage from "./pages/registerPage/RegisterPage";
 import AppProvider from "./state/AppContext";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
 
 function Router() {
