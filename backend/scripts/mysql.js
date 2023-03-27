e8c807e3174fd7f0e96fdfd3dd5fd3b0106e97ea
pkdev-git <admin@pkdevelopment.net>
2023-03-27 13:36:06 +0200

    backend fix and frontend clean

--- backend/scripts/mysql.js
+++ backend/scripts/mysql.js
@@ -12,29 +12,42 @@ const db = mysql.createPool({
 const createInitialData = () => {
   user.createUsersTable();
   const users = [
-    { email: 'szymon@example.com', password: 'password1', name: 'testowe1' },
-    { email: 'maciej@example.com', password: 'password2', name: 'testowe2' },
-    { email: 'kuba@example.com', password: 'password3', name: 'testowe3' },
-    { email: 'olek@example.com', password: 'password3', name: 'testowe4' },
+    { email: "szymon@example.com", password: "password1", name: "testowe1" },
+    { email: "maciej@example.com", password: "password2", name: "testowe2" },
+    { email: "kuba@example.com", password: "password3", name: "testowe3" },
+    { email: "olek@example.com", password: "password3", name: "testowe4" },
   ];
 
-  const insertUsersSql =
-  'INSERT INTO users (email, password, name) VALUES (?, ?, ?)';
+  const areUsersAlreadyExist = "SELECT COUNT(*) AS count FROM users";
+  db.query(areUsersAlreadyExist, (err, result) => {
+    ////////////////////////////////////
+    //dodawanie userow jezeli ich nie ma
+    ///////////////////////////////////
+    if (result[0].count === 0) {
+      const insertUsersSql =
+        "INSERT INTO users (email, password, name) VALUES (?, ?, ?)";
 
-users.forEach((user) => {
-  const checkUserSql = 'SELECT COUNT(*) AS count FROM users WHERE email = ?';
-  db.query(checkUserSql, [user.email], (err, result) => {
-  if (err) throw err;
-  if (result[0].count === 0) {
-  db.query(insertUsersSql, [user.email, user.password, user.name], (err, result) => {
-  if (err) throw err;
-  console.log(`Uzytkownik ${user.name} zostal dodany do bazy`);
-  });
-  } else {
-  console.log(`Uzytkownik ${user.name} juz istnieje w bazie`);
-  }
-  });
+      users.forEach((user) => {
+        const checkUserSql =
+          "SELECT COUNT(*) AS count FROM users WHERE email = ?";
+        db.query(checkUserSql, [user.email], (err, result) => {
+          if (result[0].count === 0) {
+            db.query(
+              insertUsersSql,
+              [user.email, user.password, user.name],
+              (err, result) => {
+                console.log(`User ${user.name} added`);
+              }
+            );
+          } else {
+            console.log(`User ${user.name} already exist in db`);
+          }
+        });
+      });
+    } else {
+      console.log("Users already exists");
+    }
   });
 };
 
-module.exports = {createInitialData};
+module.exports = { createInitialData };
