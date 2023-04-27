aba3c882913e76c924bdbeaab6d1281c51f98495
pkdev-git <admin@pkdevelopment.net>
2023-04-27 13:51:01 +0200

    async backend and homepage req

--- backend/routes.js
+++ backend/routes.js
@@ -1,38 +1,33 @@
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const Users = require("./scripts/users.js");
 const user = new Users();
 const Cors = require("./scripts/cors.js");
-const Offers = require("./scripts/offers.js");
+const Offers = require("./scripts/offers-db.js");
 const offer = new Offers();
-module.exports = function(app){
+module.exports = function (app) {
   const cors = new Cors(app);
   cors.setCors();
-  
-      app.use(bodyParser.json());
 
-      app.route("/")
-      .get((req, res) =>{
-        user.getUsers((data)=>{
-          if(data === []){
-            res.sendStatus(400);
-            res.send(data);
-          }
-          else{
-            res.sendStatus(200);
-            res.send(data);
-          }
-        })
-      });
+  app.use(bodyParser.json());
 
-      app.route("/login")
-      .post((req, res) =>{
-        user.addUser("test@test.com", "password", "Test User");
-        res.send("User added");
-      });
-      app.route("/searched")
-      .get((req, res) =>{
-        const outserch=offer.OfferSearch("Chomiki");
-        res.send(outserch);
-      });
+  app.route("/login").post((req, res) => {
+    user.addUser("test@test.com", "password", "Test User");
+    res.send("User added");
+  });
 
-}
+  app.route("/createOffer").post((req, res) => {
+    offer.addOffer("");
+
+    res.send("offer added");
+  });
+
+  app.route("/deleteOffer/:offerId").delete((req, res) => {
+    const outserch = offer.removeOffer(req.params.offerId);
+    res.send(outserch);
+  });
+  app.route("/searchProduct/:offerName").get((req, res) => {
+    offer.OfferSearch(req.params.offerName).then((response) => {
+      res.send(response);
+    });
+  });
+};
--- frontend/src/components/dashboardHeader/DashboardHeader.tsx
+++ frontend/src/components/dashboardHeader/DashboardHeader.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaUser, FaShoppingCart, FaBell } from "react-icons/fa";
 import MobileMenu from "./MobileMenu";
 import { Outlet } from "react-router-dom";
+import axios from "axios";
 
 interface HeaderProps {
   // deklarujemy propsy, których będziemy używać
@@ -26,47 +27,65 @@ const DashboardHeader: React.FC<HeaderProps> = () => {
     setshowMobileMenu(!showMobileMenu);
   };
 
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  const handleFromSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/searchProduct/${searchValue}`)
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   return (
     <>
-    <header className="main__header">
-      {/* Logo */}
-      <div className="header__logo">
-        <img src="logo.png" alt="Logo" />
-      </div>
-      {/* Wyszukiwarka z dropdownem */}
-      <div className="input__wrapper">
-        <div className="dropdown__wrapper">
-          <input
-            type="text"
-            placeholder="Szukaj"
-            value={searchValue}
-            onChange={handleSearchChange}
-            onBlur={() => setShowOptions(false)}
-            onFocus={() => setShowOptions(true)}
-          />
-          {showOptions && (
-            <ul className="header__autocomplete">
-              {filteredOptions.map((option) => (
-                <li key={option}>{option}</li>
-              ))}
-            </ul>
-          )}
+      <header className="main__header">
+        {/* Logo */}
+        <div className="header__logo">
+          <img src="logo.png" alt="Logo" />
         </div>
-      </div>
-      {/* Ikony */}
-      <div className="header__icons">
-        <FaUser />
-        <FaShoppingCart />
-        <FaBell />
-      </div>
-      <div className="header__burger" onClick={handleBurgerClick}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      {showMobileMenu && <MobileMenu callback={handleBurgerClick} />}
-    </header>
-    <Outlet/>
+        {/* Wyszukiwarka z dropdownem */}
+        <form
+          className="input__wrapper"
+          method="get"
+          onSubmit={handleFromSubmit}
+        >
+          <div className="dropdown__wrapper">
+            <input
+              type="text"
+              placeholder="Szukaj"
+              value={searchValue}
+              onChange={handleSearchChange}
+              onBlur={() => setShowOptions(false)}
+              onFocus={() => setShowOptions(true)}
+            />
+            {showOptions && (
+              <ul className="header__autocomplete">
+                {filteredOptions.map((option) => (
+                  <li key={option}>{option}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </form>
+        {/* Ikony */}
+        <div className="header__icons">
+          <FaUser />
+          <FaShoppingCart />
+          <FaBell />
+        </div>
+        <div className="header__burger" onClick={handleBurgerClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        {showMobileMenu && <MobileMenu callback={handleBurgerClick} />}
+      </header>
+      <Outlet />
     </>
   );
 };
