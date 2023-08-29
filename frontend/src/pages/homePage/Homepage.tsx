a7a3dfec01793a5d51ab0c6c63a526125d7b7ebf
sdrapinski <55097897+sdrapinski@users.noreply.github.com>
2023-08-29 19:58:21 +0200

    pages added

---
+++ frontend/src/pages/homePage/Homepage.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React, { useEffect, useContext } from "react";
+import Navbar from "../../components/navbar/Navbar";
+import { SwitchButton } from "../../components/switchButton/SwitchButton";
+import { Container } from "react-bootstrap";
+import SponsoredPosts from "../../components/Products/sponsoredProducts/SponsoredProducts";
+import Categories from "../../components/Categories/Categories";
+import MainPageProducts from "../../components/Products/mainPageProducts/MainPageProducts";
+import Footer from "../../components/footer/Footer";
+import { AppContext } from "../../state/AppContext";
+
+const Homepage = () => {
+  const appContext = useContext(AppContext);
+  useEffect(() => {
+    axios
+      .get(`${appContext?.backendUrl}/Categories`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then((resp) => {
+        console.log(resp);
+      });
+
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <Container fluid style={{ padding: "0px 30px" }}>
+        <SponsoredPosts />
+        <Categories />
+        <MainPageProducts />
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default Homepage;
---
+++ frontend/src/pages/userCart/UserCart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserCart = () => {
+  return <div>UserCart</div>;
+};
+
+export default UserCart;
