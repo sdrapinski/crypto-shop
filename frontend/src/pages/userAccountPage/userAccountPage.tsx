7f4803c82a639350cff3ddbbe64db5f20b5d1258
Ceglaa02 <jcplewiska@interia.pl>
2023-11-05 11:25:21 +0100

    next part of content in profil page

--- frontend/src/pages/userAccountPage/userAccountPage.tsx
+++ frontend/src/pages/userAccountPage/userAccountPage.tsx
@@ -35,9 +35,38 @@ const UserAccountPage = () => {
       currentButton.classList.add("active")
     }
 
+    function setOneSizeForButtons(target: string){
+      const targetContent = document.getElementById(target)
+
+      if(!targetContent){
+        return
+      }
+
+      const buttons = targetContent.querySelectorAll("button")
+
+      if(!buttons){
+        return
+      }
+
+      let width = 0
+
+      buttons.forEach((button)=>{
+        width = button.offsetWidth > width ? button.offsetWidth : width
+      })
+
+      const widthStyle = width + "px"
+
+      buttons.forEach((button)=>{
+        button.style.width = widthStyle
+      })
+    }
+
     for(const button of buttonsArray){
       button.addEventListener("click",()=>{
         changeActiveBtn(button, buttonsArray)
+        if(button.getAttribute("data-bs-target") === "#settings-content"){
+          setOneSizeForButtons("settings-content")
+        }
       })
     }
   },[])
@@ -48,8 +77,11 @@ const UserAccountPage = () => {
           <div className="d-flex align-items-center">
             <div className="nav flex-column nav-pills me-3 w-100" id="tablist" aria-orientation="vertical">
               <button className="nav-link active" id="profile-tab" data-bs-target="#profile-content" type="button">Profile</button>
-              <button className="nav-link" id="settings-tab" data-bs-target="#settings-content" type="button">Settings</button>
+              <button className="nav-link" id="userWallets-tab" data-bs-target="#userWallets-content" type="button">Wallets</button>
+              <button className="nav-link" id="userProduct-tab" data-bs-target="#userProduct-content" type="button">Your Products</button>
               <button className="nav-link" id="userCart-tab" data-bs-target="#userCart-content" type="button">Your Cart</button>
+              <button className="nav-link" id="userOrderHistory-tab" data-bs-target="#userOrderHistory-content" type="button">History of Orders</button>
+              <button className="nav-link" id="settings-tab" data-bs-target="#settings-content" type="button">Settings</button>
             </div>
           </div>
         </div>
@@ -58,52 +90,94 @@ const UserAccountPage = () => {
           <div className="tab-content">
             <div className="tab-pane fade show active" id="profile-content">
               <ol className="list-group">
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">Tokens Quantity</div>
+                    Quantity
+                  </div>
+                </li>
+                <li className="list-group-item d-flex justify-content-between align-items-start">
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">Active Wallet</div>
+                    Wallet
+                  </div>
+                </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Name</div>
-                    Content for list item
+                    Name
                   </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Surname</div>
-                    Content for list item
+                    Surname
                   </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Email</div>
-                    Content for list item
+                    Email
                   </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Login</div>
-                    Content for list item
+                    Login
                   </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Password</div>
-                    Content for list item
+                    Password
                   </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Phone</div>
-                    Content for list item
+                    Phone
                   </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Birthday</div>
-                    Content for list item
+                    Birthday
                   </div>
                 </li>
               </ol>
             </div>
-            <div className="tab-pane fade" id="settings-content">Settings</div>
+            <div className="tab-pane fade" id="settings-content">
+              <div className="row mb-4">
+                <div className="col-4">
+                  Switch Darkmode
+                </div>
+                <div className="col-4 d-flex justify-content-end">
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" role="switch"/>
+                  </div>
+                </div>
+              </div>
+              {/* <div className="row my-1">
+                <div className="col-4">
+                  History of orders
+                </div>
+                <div className="col-4 d-flex justify-content-end">
+                  <button type="button" className="btn btn-custom btn-sm">Check</button>
+                </div>
+              </div> */}
+              <div className="row my-1">
+                <div className="col-4">
+                  Edit data
+                </div>
+                <div className="col-4 d-flex justify-content-end">
+                  <button type="button" className="btn btn-custom btn-sm">Edit</button>
+                </div>
+              </div>
+            </div>
             <div className="tab-pane fade" id="userCart-content">Cart</div>
+            <div className="tab-pane fade" id="userProduct-content">Products</div>
+            <div className="tab-pane fade" id="userWallets-content">Wallets</div>
+            <div className="tab-pane fade" id="userOrderHistory-content">History of Orders</div>
           </div>
         </div>
       </div>
