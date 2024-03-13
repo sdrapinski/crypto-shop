2df2b7f6e793b58b376541f205e1ccca303b4596
Ceglaa02 <jcplewiska@interia.pl>
2024-03-13 14:39:50 +0100

    user orders frontend prepare and code refactor

--- frontend/src/pages/userAccountPage/subPages/UserOrders.tsx
+++ frontend/src/pages/userAccountPage/subPages/UserOrders.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const UserOrders = () => {
-  return <div className="tab-pane fade show active">UserOrders</div>;
+  return <div className="userorders-content tab-pane fade show active">
+    <div><p>#1 order</p><button className="view">View</button></div>
+    <div><p>#2 order</p><button className="view">View</button></div>
+    <div><p>#3 order</p><button className="view">View</button></div>
+    <div><p>#4 order</p><button className="view">View</button></div>
+    <div><p>#5 order</p><button className="view">View</button></div>
+    <div><p>#6 order</p><button className="view">View</button></div>
+  </div>;
 };
