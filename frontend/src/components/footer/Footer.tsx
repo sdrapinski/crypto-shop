97851611b361c68b51d9b94a8661446ae1131533
sdrapinski <szymondrapinski@gmail.com>
2024-10-29 23:11:12 +0100

    footer content

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+        
+          <Col md={4} className="footer-section">
+            <h5>O nas</h5>
+            <p>
+              Projekt inżynierski pionierskiego sklepu internetowego umożliwiającego wiele sposobów płatności.
+              Naszym celem jest dostarczenie innowacyjnych rozwiązań zakupowych.
+            </p>
+          </Col>
+
+          
+          <Col md={4} className="footer-section">
+            <h5>Zespół</h5>
+            <ul className="footer-links">
+              <li>Szymon Drapiński</li>
+              <li>Maciej Adamski</li>
+              <li>Jakub Cegłowski</li>
+            </ul>
+          </Col>
+         
+        </Row>
+        <Row className="footer-bottom">
+          <Col className="text-center">
+            <p>&copy; {new Date().getFullYear()} CryptoShop. Wszelkie prawa zastrzeżone.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 };
 
 export default Footer;
